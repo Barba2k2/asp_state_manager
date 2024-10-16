434d9ea5d568139c17cb855c4a47b48a607b9640
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-10-16 01:02:52 -0300

    refactor(shopping): update ShoppingListState to use ShoppingItem entity class

--- lib/src/features/shopping/state/shopping_list_state.dart
+++ lib/src/features/shopping/state/shopping_list_state.dart
@@ -1,16 +1,20 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../entities/shopping_item_entitie.dart';
 
 // Estado da Lista de Compras
 class ShoppingListState {
-  final List<String> items;
+  final List<ShoppingItem> items;
   ShoppingListState({this.items = const []});
 
-  ShoppingListState addItem(String item) {
+  ShoppingListState addItem(ShoppingItem item) {
     return ShoppingListState(items: [...items, item]);
   }
 
-  ShoppingListState removeItem(String item) {
-    return ShoppingListState(items: items.where((i) => i != item).toList());
+  ShoppingListState removeItem(ShoppingItem item) {
+    return ShoppingListState(
+        items: items.where((i) => i.name != item.name).toList());
   }
 }
 
@@ -20,13 +24,43 @@ final shoppingListProvider =
 );
 
 class ShoppingListNotifier extends StateNotifier<ShoppingListState> {
-  ShoppingListNotifier() : super(ShoppingListState());
-
-  void addItem(String item) {
-    state = state.addItem(item);
+  ShoppingListNotifier() : super(ShoppingListState()) {
+    _loadItems();
   }
 
-  void removeItem(String item) {
+  void addItem(ShoppingItem item) async {
+    state = state.addItem(item);
+    await _saveItems();
+  }
+
+  void removeItem(ShoppingItem item) async {
     state = state.removeItem(item);
+    await _saveItems();
+  }
+
+  Future<void> _loadItems() async {
+    final prefs = await SharedPreferences.getInstance();
+    final List<String>? items = prefs.getStringList('shopping_list');
+    if (items != null) {
+      final parsedItems = items.map(
+        (item) {
+          final split = item.split('::');
+          return ShoppingItem(name: split[0], category: split[1]);
+        },
+      ).toList();
+
+      state = ShoppingListState(items: parsedItems);
+    }
+  }
+
+  Future<void> _saveItems() async {
+    final prefs = await SharedPreferences.getInstance();
+    final List<String> itemsToSave = state.items
+        .map(
+          (item) => '${item.name}::${item.category}',
+        )
+        .toList();
+
+    await prefs.setStringList('shopping_list', itemsToSave);
   }
 }
