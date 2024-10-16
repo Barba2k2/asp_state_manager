8f4e89b41e4b766268e763609609b69d3aa6385f
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-10-16 01:34:43 -0300

    refactor(main): Update main.dart to include database and notification initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 import 'src/features/shopping/ui/shopping_list_screen.dart';
+import 'src/helper/notification_helper.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NotificationsHelper.initializeNotifications();
+  sqfliteFfiInit();
+  databaseFactory = databaseFactoryFfi;
+
   runApp(
     const ProviderScope(
       child: MyApp(),
@@ -16,8 +23,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ShoppingListScreen(),
+    return MaterialApp(
+      title: 'Lista de Compras Inteligente',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: false,
+      ),
+      home: const ShoppingListScreen(),
     );
   }
 }
