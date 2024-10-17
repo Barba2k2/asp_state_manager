bbf87f9e584f583a9643dd6388e9519aecdb66e4
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-10-17 15:36:38 -0300

    refactor(entities): Update ShoppingItem class with id field and copyWith method

--- lib/src/domain/entities/shopping_item.dart
+++ lib/src/domain/entities/shopping_item.dart
@@ -1,4 +1,5 @@
 class ShoppingItem {
+  final int? id;
   final String name;
   final String category;
   final bool isUrgent;
@@ -7,6 +8,7 @@ class ShoppingItem {
   final String priceType;
 
   ShoppingItem({
+    this.id,
     required this.name,
     required this.category,
     this.isUrgent = false,
@@ -16,40 +18,47 @@ class ShoppingItem {
   });
 
   ShoppingItem toggleUrgent() {
-    return ShoppingItem(
-      name: name,
-      category: category,
-      isUrgent: !isUrgent,
-      purchaseCount: purchaseCount,
-      price: price,
-      priceType: priceType,
-    );
+    return copyWith(isUrgent: !isUrgent);
   }
 
   ShoppingItem incrementPurchaseCount() {
+    return copyWith(purchaseCount: purchaseCount + 1);
+  }
+
+  ShoppingItem copyWith({
+    int? id,
+    String? name,
+    String? category,
+    bool? isUrgent,
+    int? purchaseCount,
+    double? price,
+    String? priceType,
+  }) {
     return ShoppingItem(
-      name: name,
-      category: category,
-      isUrgent: isUrgent,
-      purchaseCount: purchaseCount + 1,
-      price: price,
-      priceType: priceType,
+      id: id ?? this.id,
+      name: name ?? this.name,
+      category: category ?? this.category,
+      isUrgent: isUrgent ?? this.isUrgent,
+      purchaseCount: purchaseCount ?? this.purchaseCount,
+      price: price ?? this.price,
+      priceType: priceType ?? this.priceType,
     );
   }
 
   String toSerializedString() {
-    return '$name::$category::$isUrgent::$purchaseCount::${price ?? 0.0}::$priceType';
+    return '$id::$name::$category::$isUrgent::$purchaseCount::${price ?? 0.0}::$priceType';
   }
 
   static ShoppingItem fromSerializedString(String serialized) {
     final split = serialized.split('::');
     return ShoppingItem(
-      name: split[0],
-      category: split[1],
-      isUrgent: split[2] == 'true',
-      purchaseCount: int.parse(split[3]),
-      price: double.parse(split[4]),
-      priceType: split[5],
+      id: split[0].isNotEmpty ? int.tryParse(split[0]) : null,
+      name: split[1],
+      category: split[2],
+      isUrgent: split[3] == 'true',
+      purchaseCount: int.parse(split[4]),
+      price: double.parse(split[5]),
+      priceType: split[6],
     );
   }
 }
