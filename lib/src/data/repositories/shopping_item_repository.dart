e635b0043320602cf65d6ab568addde8c1bfb1df
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-10-17 15:36:47 -0300

    Refactor shopping item repository to include id field in database operations

--- lib/src/data/repositories/shopping_item_repository.dart
+++ lib/src/data/repositories/shopping_item_repository.dart
@@ -4,32 +4,39 @@ import '../datasources/database_helper.dart';
 class ShoppingItemRepository {
   final DatabaseHelper _databaseHelper = DatabaseHelper();
 
-  Future<void> addItem(ShoppingItem item) async {
+  Future<int> addItem(ShoppingItem item) async {
     final db = await _databaseHelper.database;
-    await db.insert('shopping_items', {
-      'name': item.name,
-      'category': item.category,
-      'isUrgent': item.isUrgent ? 1 : 0,
-      'purchaseCount': item.purchaseCount,
-      'price': item.price ?? 0.0,
-      'priceType': item.priceType,
-    });
+    return await db.insert(
+      'shopping_items',
+      {
+        'name': item.name,
+        'category': item.category,
+        'isUrgent': item.isUrgent ? 1 : 0,
+        'purchaseCount': item.purchaseCount,
+        'price': item.price ?? 0.0,
+        'priceType': item.priceType,
+      },
+    );
   }
 
   Future<List<ShoppingItem>> getItems() async {
     final db = await _databaseHelper.database;
     final List<Map<String, dynamic>> maps = await db.query('shopping_items');
 
-    return List.generate(maps.length, (i) {
-      return ShoppingItem(
-        name: maps[i]['name'],
-        category: maps[i]['category'],
-        isUrgent: maps[i]['isUrgent'] == 1,
-        purchaseCount: maps[i]['purchaseCount'],
-        price: maps[i]['price'],
-        priceType: maps[i]['priceType'],
-      );
-    });
+    return List.generate(
+      maps.length,
+      (i) {
+        return ShoppingItem(
+          id: maps[i]['id'],
+          name: maps[i]['name'],
+          category: maps[i]['category'],
+          isUrgent: maps[i]['isUrgent'] == 1,
+          purchaseCount: maps[i]['purchaseCount'],
+          price: maps[i]['price'],
+          priceType: maps[i]['priceType'],
+        );
+      },
+    );
   }
 
   Future<void> updateItem(ShoppingItem item) async {
@@ -37,6 +44,7 @@ class ShoppingItemRepository {
     await db.update(
       'shopping_items',
       {
+        'id': item.id,
         'name': item.name,
         'category': item.category,
         'isUrgent': item.isUrgent ? 1 : 0,
@@ -49,12 +57,12 @@ class ShoppingItemRepository {
     );
   }
 
-  Future<void> removeItem(String name) async {
+  Future<void> removeItem(int id) async {
     final db = await _databaseHelper.database;
     await db.delete(
       'shopping_items',
-      where: 'name = ?',
-      whereArgs: [name],
+      where: 'id = ?',
+      whereArgs: [id],
     );
   }
 }
