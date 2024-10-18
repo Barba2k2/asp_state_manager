b519480a86afae39a928e0ce903cdf25300e8f5f
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-10-17 21:02:07 -0300

    Refactor AddItemForm to include logging statements for category and price selection, and item addition

--- lib/src/presentation/widgets/add_item_form.dart
+++ lib/src/presentation/widgets/add_item_form.dart
@@ -1,8 +1,10 @@
 import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../domain/entities/shopping_item.dart';
-import '../../features/shopping/state/shopping_list_state.dart';
+// import '../../features/shopping/state/shopping_list_state.dart';
+import '../../application/notifiers/shopping_list_notifier.dart';
 
 class AddItemForm extends ConsumerStatefulWidget {
   const AddItemForm({super.key});
@@ -12,29 +14,32 @@ class AddItemForm extends ConsumerStatefulWidget {
 }
 
 class _AddItemFormState extends ConsumerState<AddItemForm> {
-  final TextEditingController itemController = TextEditingController();
-  final TextEditingController priceController = TextEditingController();
-  String selectedCategory = 'Hortifruti';
-  String selectedPriceType = 'Unidade';
+  final _itemController = TextEditingController();
+  final _priceController = TextEditingController();
+  var selectedCategory = 'Hortifruti';
+  var selectedPriceType = 'Unidade';
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         TextField(
-          controller: itemController,
+          controller: _itemController,
           decoration: const InputDecoration(
             labelText: 'Novo item',
           ),
         ),
         TextField(
-          controller: priceController,
+          controller: _priceController,
+          inputFormatters: [
+            FilteringTextInputFormatter.allow(RegExp(r'^\d+[,.]?\d{0,2}')),
+          ],
           decoration: const InputDecoration(
             labelText: 'Preço do Item',
           ),
-          keyboardType: TextInputType.number,
+          keyboardType: const TextInputType.numberWithOptions(decimal: true),
         ),
-        DropdownButton<String>(
+        DropdownButton(
           value: selectedCategory,
           items: ['Hortifruti', 'Laticínios', 'Limpeza', 'Outros']
               .map(
@@ -53,7 +58,7 @@ class _AddItemFormState extends ConsumerState<AddItemForm> {
             }
           },
         ),
-        DropdownButton<String>(
+        DropdownButton(
           value: selectedPriceType,
           items: ['Unidade', 'Quilo', 'Bandeja', 'Litro']
               .map(
@@ -74,10 +79,10 @@ class _AddItemFormState extends ConsumerState<AddItemForm> {
         ),
         ElevatedButton(
           onPressed: () async {
-            final newItemName = itemController.text.trim();
+            final newItemName = _itemController.text.trim();
             final newItemPrice =
-                double.tryParse(priceController.text.trim()) ?? 0.0;
-
+                double.tryParse(_priceController.text.trim()) ?? 0.0;
+        
             if (newItemName.isNotEmpty) {
               final newItem = ShoppingItem(
                 name: newItemName,
@@ -85,12 +90,19 @@ class _AddItemFormState extends ConsumerState<AddItemForm> {
                 price: newItemPrice,
                 priceType: selectedPriceType,
               );
-              log('Tentando adicionar item: ${newItem.name}, Preço: ${newItem.price}, Categoria: ${newItem.category}, Tipo de Preço: ${newItem.priceType}');
-              await ref.read(shoppingListProvider.notifier).addItem(newItem);
+              log('''
+                Tentando adicionar item: ${newItem.name}
+                Preço: ${newItem.price}
+                Categoria: ${newItem.category}
+                Tipo de Preço: ${newItem.priceType}
+              ''');
+              await ref
+                  .read(shoppingListProvider.notifier)
+                  .addItem(newItem);
               log('Item adicionado com sucesso e estado deve ser atualizado agora.');
-
-              itemController.clear();
-              priceController.clear();
+        
+              _itemController.clear();
+              _priceController.clear();
             }
           },
           child: const Text('Adicionar Item'),
